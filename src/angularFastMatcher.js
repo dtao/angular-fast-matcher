a9b63d88fcfdda5f515c5533f93b6047e1683caa
Dan Tao <daniel.tao@gmail.com>
2014-03-25 16:07:18 -0700

    added keyboard selection
    The current implementation is not really acceptable, though. It pollutes the
    parent scope with a 'selectedIndex' property. This should be configurable.

--- src/angularFastMatcher.js
+++ src/angularFastMatcher.js
@@ -8,11 +8,14 @@
     return {
       scope: {
         source: '=fastMatcherSource',
-        matches: '=fastMatcherOutput'
+        matches: '=fastMatcherOutput',
+        selector: '=fastMatcherSelector'
       },
       link: function(scope, element, attrs) {
-        var selector = attrs.fastMatcherProperty,
-            needleSelector = attrs.ngModel;
+        var parentScope = scope.$parent,
+            property = attrs.fastMatcherProperty,
+            selector = scope.selector,
+            model = attrs.ngModel;
 
         if (!scope.matches) {
           scope.matches = [];
@@ -20,19 +23,49 @@
 
         $q.when(scope.source).then(function(source) {
           var matcher = new FastMatcher(source, {
-            selector: selector,
+            selector: property,
             matches: scope.matches
           });
 
-          scope.$parent.$watch(needleSelector, function() {
-            var needle = scope.$parent[needleSelector];
+          parentScope.$watch(model, function() {
+            var needle = parentScope[model];
 
             scope.matches.length = 0;
+            parentScope.selectedIndex = -1;
 
             if (needle) {
               matcher.getMatches(needle);
             }
           });
+
+          if (selector) {
+            element.on('keyup', function(e) {
+              switch (e.keyCode) {
+                case 13: // enter
+                  if (parentScope.selectedIndex >= 0 && parentScope.selectedIndex < scope.matches.length) {
+                    selector(scope.matches[parentScope.selectedIndex]);
+                  } else if (scope.matches.length === 1) {
+                    selector(scope.matches[0]);
+                  }
+                  parentScope.selectedIndex = -1;
+                  break;
+
+                case 27: // esc
+                  element.blur();
+                  break;
+
+                case 38: // up
+                  parentScope.selectedIndex = (parentScope.selectedIndex || scope.matches.length) - 1;
+                  break;
+
+                case 40: // down
+                  parentScope.selectedIndex = ++parentScope.selectedIndex % scope.matches.length;
+                  break;
+              }
+
+              scope.$apply();
+            });
+          }
         });
       }
     };
